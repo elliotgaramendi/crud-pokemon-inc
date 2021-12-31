679374ca619729c24f68ca26f33508fa5f01651e
ElliotXLeo <elliotgaramendi@gmail.com>
2021-12-31 15:06:25 -0500

    backend: Database connection

--- backend/models/Conexion.php
+++ backend/models/Conexion.php
@@ -1,1 +1,16 @@
 <?php
+
+define('SERVIDOR', 'localhost');
+define('NOMBRE_BD', 'develop');
+define('USUARIO', 'root');
+define('CLAVE', '');
+$opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+
+try {
+  $pdo = new PDO('mysql:host=' . SERVIDOR . ';dbname=' . NOMBRE_BD, USUARIO, CLAVE, $opciones);
+  return $pdo;
+} catch (Exception $e) {
+  $respuesta = ['Error' => 'error: ' . $e->getMessage()];
+  print json_encode($respuesta);
+  die();
+}
