ee59c22b18e23708f0246a194d332fcb41aa56e7
ElliotXLeo <elliotgaramendi@gmail.com>
2021-12-31 15:10:59 -0500

    frontend: separation of index into components

---
+++ frontend/components/index-apertura.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD Usuarios Pokémon Inc</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+  <header class="bg-primary text-light text-center">
+    <h1>CRUD Usuarios Pokémon Inc</h1>
+  </header>
--- frontend/index.php
+++ frontend/index.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php require_once './components/index-apertura.php' ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CRUD Pokémon Inc</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body>
-  <header class="bg-primary text-light text-center">
-    <h1>CRUD Pokémon Inc</h1>
-  </header>
-  <footer class="bg-dark text-light text-center fixed-bottom p-2">
-    <h6>
-      Desarrollado por Elliot Garamendi
-    </h6>
-  </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php require_once './components/index-cierre.php' ?>
